0ac0b09a072f1bfaccef122e2f61c957fe8f904e
Melo705 <909463823@qq.com>
2023-04-04 21:25:14 +0800

    add queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,30 @@
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int queue_pop(struct m_queue* q, int* ele)
+{
+
+}
+
+int queue_push(struct m_queue* q, int ele)
+{
+
+}
+
+int queue_init(struct m_queue* q, int size)
+{
+
+}
+
+struct m_queue* mc_queue_clear(struct m_queue* q)
+{
+
+}
+
+struct m_queue* mc_queue_destroy(struct m_queue* q)
+{
+
+}
+
